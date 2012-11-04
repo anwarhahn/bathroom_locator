77460d4a9bcadfa8723d21e0b09b60d4716ff827
Anwar Hahn <ahahn89@gmail.com>
2012-11-04 13:21:27 -0800

    Not using local jquery file anymore. Using online version instead. This way we get the jquery icons to show up properly. Also added distance sorting to list view.

--- pages/list.php
+++ pages/list.php
@@ -13,29 +13,60 @@
 	</head>
 	<body>
 		<script type='text/javascript'>
-			var b = <?= $data; ?>;
+			
+			// default to stanford center
 			var stanfordLatLng = new google.maps.LatLng(37.428729,-122.171329);
-			var list = [];	
-			for(var i in b) {
-				var bathroom = b[i];
-				var loc = new google.maps.LatLng(bathroom.latitude, bathroom.longitude);
-				var dist = google.maps.geometry.spherical.computeDistanceBetween(stanfordLatLng, loc);
-				var li = document.createElement("li");
-				var a = document.createElement("a");
-				a.innerHTML = "<h3>" + bathroom.name + "</h3><p>Extra info</p>";
-				li.appendChild(a);
-				list.push(li);
+			
+			var makeList = function(position) {
+				var list = [];	
+				var bathroom_data = <?= $data; ?>;
+				var bList = [];
+				for(var i in bathroom_data) {
+					var bathroom = bathroom_data[i];
+					var loc = new google.maps.LatLng(bathroom.latitude, bathroom.longitude);
+					bathroom.dist = google.maps.geometry.spherical.computeDistanceBetween(position, loc);
+					bList.push(bathroom);
+				}
+				bList.sort(function(a, b) { return (a.dist - b.dist); });
 
-				$(a).click((function(name) {
-						return function() { window.location = "specificBathroom.php?origin=list&name=" + name; }
-					})(escape(bathroom.name)));
+				for(var i = 0; i < bList.length; i++) {
+					var bathroom = bList[i];
+					var li = document.createElement("li");
+					var a = document.createElement("a");
+					a.innerHTML = "<h3>" + bathroom.name + "</h3><p>"+bathroom.dist.toPrecision(4)+" meters</p>";
+					li.appendChild(a);
+					list.push(li);
+
+					$(a).click((function(name) {
+							return function() { window.location = "specificBathroom.php?origin=list&name=" + name; }
+						})(escape(bathroom.name)));
+				}
+				return list;
 			}
-		
+
 			$(document).bind('pageinit', function(event) {
-				var ul = $("<ul>");
-				$("#home").append(ul);
-				$("#bathroom_list").append(list).listview();
+				if (navigator.geolocation) {
+					var success = function(position) {
+						latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+						var list = makeList(latLng);
+						//console.log(list);
+						var ul = $("<ul>");
+						$("#home").append(ul);
+						$("#bathroom_list").append(list).listview();
+					}
+
+					navigator.geolocation.getCurrentPosition(success, function() {});
+
+				} else {
+					var list = makeList(stanfordLatLng);
+					//console.log(list);
+					var ul = $("<ul>");
+					$("#home").append(ul);
+					$("#bathroom_list").append(list).listview();
+				}
 			});			
+
+			
 		</script>
 
 
