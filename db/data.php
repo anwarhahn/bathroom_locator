44198d84b7a016aae135da99c028d9e95344659a
Anwar Hahn <ahahn89@gmail.com>
2012-11-08 23:49:40 -0800

    fixed problem in data.php with default array parameter

--- db/data.php
+++ db/data.php
@@ -33,8 +33,8 @@ class Data {
 		return $this->$array;
 	}
 
-	function all_json($arr = []) {
-		if ($arr == []) $arr = $this->array;
+	function all_json($arr = array()) {
+		if ($arr == array()) $arr = $this->array;
 		return json_encode($arr);
 	}
 
